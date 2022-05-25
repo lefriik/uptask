b32e8342d94db9410998f0d1a76e0a9a48022c08
lefriik <emerio.isaac.suarez@gmail.com>
2022-05-25 11:28:09 -0400

    modelo usuario

---
+++ models/Usuario.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose"
+
+// schema es la estructura de la base de datos
+
+const usuarioSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    token: {
+        type: String,
+    },
+    confirmado: {
+        type: Boolean,
+        default: false,
+    },
+},{
+    timestamps: true, //crea 2 columnas (creado y actualizado)
+} );
+
+const Usuario = mongoose.model("Usuario", usuarioSchema);
+export default Usuario;
