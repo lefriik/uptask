5c4e277fb8161fdca61ab66c4d300b9aaaf9a2d4
lefriik <emerio.isaac.suarez@gmail.com>
2022-07-06 14:44:08 -0400

    schema tareas

--- models/Proyecto.js
+++ models/Proyecto.js
@@ -25,6 +25,12 @@ creador: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Usuario",
 },
+tareas: [
+   {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tarea'
+   } 
+],
 colaboradores: [
     {
         type: mongoose.Schema.Types.ObjectId,
